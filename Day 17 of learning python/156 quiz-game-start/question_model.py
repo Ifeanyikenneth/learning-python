f0df0604d13e24e7db29fa827459db2317ffe127
Ifeanyi kenneth <ifeanyikenneth005@gmail.com>
2026-02-09 05:30:42 +0100

    pushing Day 17 of learning python

---
+++ Day 17 of learning python/156 quiz-game-start/question_model.py
@@ -0,0 +1,16 @@
+class Question:
+    def __init__(self, text , answer):
+        self.text = text
+        self.answer = answer
+
+
+# class Car:
+#     def __init__(self, year, color):
+#         self.year = year
+#         self.color = color
+# car = Car("2008", "blue")
+
+# print(car.year, car.color)
+
+
+
