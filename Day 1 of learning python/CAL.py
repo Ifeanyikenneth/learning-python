bd67c2af584261c6e0e5b77abc55eee7610bedfe
Ifeanyi kenneth <ifeanyikenneth005@gmail.com>
2026-02-22 13:45:31 +0100

    pushing Day 18 of learning python

--- Day 1 of learning python/CAL.py
+++ Day 1 of learning python/CAL.py
@@ -8,9 +8,19 @@ print("sum: {} + {} = {}".format(a,b,a+b))
 print("Difference: {} - {} = {}".format(a,b,a-b))
 
 
-f = a
-a = b
-b = f
+# f = a
+# a = b
+# b = f
+
+ 
 
 print("a: " + str(a))
 print("a: " + str(b))
+
+
+
+# a = int(input ("Enter the first number: "))
+# b = int(input ("Enter the second number: "))
+
+# print(a + b)
+# print(a - b)
